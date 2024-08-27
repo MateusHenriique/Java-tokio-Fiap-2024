fbe2d6a1e30db07c35aea3f3eb10cc628a54df1d
Mateus Henrique Da Silva <mateush5453@gmail.com>
2024-08-27 17:53:39 -0300

    primeiro commit

---
+++ javaTokio2024/src/logica/exercicios/aula04/EX06.java
@@ -0,0 +1,18 @@
+package logica.exercicios.aula04;
+
+import java.util.Scanner;
+
+public class EX06 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Qual e o seu nome: ");
+		System.out.println("Ola," + input.nextLine());
+		
+		input.close();
+
+	}
+
+}
