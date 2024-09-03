fd331d289c831d25771a6573925f1546f42ccaf9
MateusHenriique <Mateush5453@gmail.com>
2024-09-03 00:32:50 -0300

    finalizei

---
+++ javaTokio2024/src/logica/exercicios/aula06/Ex04.java
@@ -0,0 +1,25 @@
+package logica.exercicios.aula06;
+
+import java.util.Scanner;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Digite o 1 numero: ");
+		int number1 = Integer.parseInt(input.nextLine());
+		System.out.print("Digite p 2 numero: ");
+		int number2 = Integer.parseInt(input.nextLine());
+		
+		if (number1 % number2 == 0) {
+			System.out.println("Sao Multipos");
+		}else {
+			System.out.println("Nao sao Multiplos");
+		}
+		
+		input.close();
+
+	}
+
+}
