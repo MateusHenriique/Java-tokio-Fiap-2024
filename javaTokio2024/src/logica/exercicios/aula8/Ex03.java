de2f42d9dc30dd58812899daff9aa298e68b0c44
MateusHenriique <Mateush5453@gmail.com>
2024-09-05 02:23:04 -0300

    falta pouco para finalizar

---
+++ javaTokio2024/src/logica/exercicios/aula8/Ex03.java
@@ -0,0 +1,40 @@
+package logica.exercicios.aula8;
+
+import java.util.Scanner;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in); 
+		
+		System.out.println("Digite algo: ");
+		String algo = input.next();
+		int totMaiusc = 0, totMinusc = 0, totNumeric = 0, totSpace = 0;
+		for (int i = 0; i < algo.length(); i++) {
+			
+			if (algo.charAt(i) >= 'A' && algo.charAt(i) <= 'Z') {
+				totMaiusc++;
+			}
+			
+			if (algo.charAt(i) >= 'a' && algo.charAt(i) <= 'z') {
+				totMinusc++;
+			}
+			
+			if (algo.charAt(i) >= '0' && algo.charAt(i) <= '9') {
+				totNumeric++;
+			}
+			
+			if (algo.charAt(i) == ' ') {
+				totSpace++;
+				System.out.println(totSpace);
+			}
+		}
+		
+		System.out.printf("A quantidade de caracteres alfabeticos maiusculos foi: %d \n", totMaiusc);
+		System.out.printf("A quantidade de caracteres alfabeticos minusculos foi: %d \n", totMinusc);
+		System.out.printf("A quantidade de caracteres numericos foi: %d \n", totNumeric);
+		System.out.printf("A quantidade de espaco em branco foi %d \n", totSpace);
+		
+	}
+
+}
